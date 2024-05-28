fb39a64539a6dfa95b701eb9f7401b0467f84b58
JsRkll132 <jose.apaza2@unmsm.edu.pe>
2024-05-28 15:38:29 -0500

    delpoyed conecction

--- initflask.py
+++ initflask.py
@@ -1,8 +1,12 @@
 from dbinstance import db
 from flask import Flask
+#from dotenv import load_dotenv
 import os
-
+#load_dotenv()
 def create_app() : 
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI']= 'postgres://jsrkll132:C5rlOPDIITGyytFNjFvCtMZyteO0k6kR@dpg-cpagk1lds78s73d1tc9g-a/sisvita_db_g4'
+    app.config['SQLALCHEMY_DATABASE_URI']= "postgresql://jsrkll132:GRyZwb4RJJxzcTkU8STWWsaXIlejldHo@dpg-cpb3sqvjbltc73esea30-a.oregon-postgres.render.com/dsm_241_g4"
     db.init_app(app)
+
+
+create_app()
