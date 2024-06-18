31e1868a955d7bf8b1caa95a98cf75927e48a846
JsRkll132 <jose.apaza2@unmsm.edu.pe>
2024-06-18 02:22:00 -0500

    .

--- sisvitapp/models/dbModel.py
+++ sisvitapp/models/dbModel.py
@@ -81,20 +81,16 @@ class Diagnosticos(Base):
     calificacion = Column(Integer, nullable=False)
     diagnostico = Column(Text, nullable=False)
     fecha_diagnostico = Column(TIMESTAMP, server_default=func.current_timestamp(), nullable=False)
-class PuntajesFormulario(Base):
-    __tablename__ = 'puntajesformulario'
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    puntaje = Column(Integer, nullable=False)
-    formulario_id = Column(Integer, ForeignKey('formularios.id'), nullable=False)
 
-    formulario = relationship("Formularios", backref="puntajesformulario")
-class RespuestaFormulario(Base):
-    __tablename__ = 'respuestaformulario'
+
+class ContenidoFormulario(Base):
+    __tablename__ = 'contenidoformulario'
     id = Column(Integer, primary_key=True, autoincrement=True)
     respuestaformulario = Column(String(255), nullable=False)
+    puntaje = Column(Integer, nullable=False)
     formulario_id = Column(Integer, ForeignKey('formularios.id'), nullable=False)
+    formulario = relationship("Formularios", backref="contenidoformulario")
 
-    formulario = relationship("Formularios", backref="respuestaformulario")
 class CompletadoFormulario(Base):
     __tablename__ = 'completadoformulario'
     id = Column(Integer, primary_key=True, autoincrement=True)
